6000a2ac1ab1dff8f9187bcc1f638017c1fd748c
Moonminji <wkdsu7@naver.com>
2020-11-13 16:25:27 +0900

    2020.11.13

---
+++ thread_mutex_1.c
@@ -0,0 +1,45 @@
+/**
+ * @file 	thread_mutex.c
+ * @brief 	
+ * @author 	minji (minji@nasmedia.co.kr)
+ * @date 	2020년 11월 13일
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+ 
+// 뮤텍스 객체 선언
+pthread_mutex_t mutex_lock;
+ 
+int g_count = 0;  // 쓰레드 공유자원.
+ 
+void *t_function(void *data)
+{
+    int i;
+    char* thread_name = (char*)data;
+    // critical section
+    g_count = 0;   // 쓰레드마다 0부터 시작.
+    for (i = 0; i < 3; i++)
+    {
+        printf("%s COUNT %d\n", thread_name, g_count);
+        g_count++;  // 쓰레드 공유자원, 1증가.
+        sleep(1);
+    }
+}
+int main()
+{
+    pthread_t p_thread1, p_thread2;
+    int status;
+ 
+    pthread_create(&p_thread1, NULL, t_function, (void *)"Thread1");
+    pthread_create(&p_thread2, NULL, t_function, (void *)"Thread2");
+ 
+    pthread_join(p_thread1, (void *)&status);
+    pthread_join(p_thread2, (void *)&status);
+}
+
+
+
+// 이 코드를 돌릴 땐 gcc -o 파일명 파일명.c -lpthread 해줘야함.
